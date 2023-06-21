c4ff3668d0404b829848bc231301c08766087ca3
xavier-kong <kongxianchengxavier@gmail.com>
2023-06-21 08:54:21 +0800

    completed up to threads1

--- exercises/smart_pointers/box1.rs
+++ exercises/smart_pointers/box1.rs
@@ -18,11 +18,10 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -31,15 +30,15 @@ fn main() {
     println!(
         "This is a non-empty cons list: {:?}",
         create_non_empty_list()
-    );
+        );
 }
 
 pub fn create_empty_list() -> List {
-    todo!()
+    List::Nil
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    List::Cons(1, Box::new(List::Nil))
 }
 
 #[cfg(test)]
--- exercises/threads/threads2.rs
+++ exercises/threads/threads2.rs
@@ -34,6 +34,6 @@ fn main() {
         // TODO: Print the value of the JobStatus.jobs_completed. Did you notice
         // anything interesting in the output? Do you have to 'join' on all the
         // handles?
-        println!("jobs completed {}", ???);
+        println!("jobs completed {}", status.jobs_completed);
     }
 }
